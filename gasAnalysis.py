6a28da09bef252671135fd15e2a3292d65d2a238
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-24 16:34:41 +0100

    reading processed file condition

--- gasAnalysis.py
+++ gasAnalysis.py
@@ -1,4 +1,5 @@
 import sys,os,time,stat,logging
+import pathlib
 import pandas as pd
 import numpy as np
 logging.basicConfig(level=logging.INFO)
@@ -9,6 +10,13 @@ from prep_data import *
 def main():
     #load data
     print("load data")
+    name="2020/01/2020-01-01-prices.csv"
+    my_file = pathlib.Path("data/processed/"+name)
+    if not my_file.exists ():
+        print("file does NOT exist: require processing")
+    else:
+        prices=pd.read_csv(my_file)
+        print("Read {} entries for {}".format(len(prices),my_file))
     
 if __name__ == "__main__":
     start = time.time()
