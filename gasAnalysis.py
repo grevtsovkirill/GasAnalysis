e8d723af5962c9bb248178b0bd85d3a1fda5ee74
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-24 16:46:02 +0100

    call funtion to process data for selected events

--- gasAnalysis.py
+++ gasAnalysis.py
@@ -13,10 +13,14 @@ def main():
     name="2020/01/2020-01-01-prices.csv"
     my_file = pathlib.Path("data/processed/"+name)
     if not my_file.exists ():
-        print("file does NOT exist: require processing")
+        print("file does NOT exist: require processing (save output to processed folder)")
+        prices_raw=pd.read_csv("data/raw/"+name)
+        prices = presel_data(prices_raw)        
+        prices.to_csv(my_file)
     else:
         prices=pd.read_csv(my_file)
-        print("Read {} entries for {}".format(len(prices),my_file))
+        
+    print("Read {} entries for {}".format(len(prices),my_file))
     
 if __name__ == "__main__":
     start = time.time()
