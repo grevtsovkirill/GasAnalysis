802da44e530c73acd9ab2c272b24838ce697642c
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-24 10:56:32 +0100

    production version

---
+++ gasAnalysis.py
@@ -0,0 +1,17 @@
+import sys,os,time,stat,logging
+import pandas as pd
+import numpy as np
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+
+def main():
+    #load data
+    print("load data")
+    
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    log.info("Process time: %f" %(time.time() - start))
+
