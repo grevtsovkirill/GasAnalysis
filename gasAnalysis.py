3acabd95e71c62596058a3f658be690a04daeada
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-04 18:14:25 +0100

    save overview of price change in period

--- gasAnalysis.py
+++ gasAnalysis.py
@@ -1,17 +1,19 @@
 import sys,os,time,stat,logging
-import pathlib
 import pandas as pd
 import numpy as np
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 from prep_data import *
-
+from plot_helper import *
 
 def main():
     #load data
-    print("load data")
+    #print("prepare data in the city")    
+    #prepare_data()
+    #select data for given station
     prices=give_me_data()
+    plot_over_time(prices)
     
 if __name__ == "__main__":
     start = time.time()
