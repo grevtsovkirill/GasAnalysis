5b96271049732833bda7ee0bd818259c408edd5b
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-04 16:53:52 +0100

    move data creation module to prep file

--- gasAnalysis.py
+++ gasAnalysis.py
@@ -7,39 +7,12 @@ log = logging.getLogger(__name__)
 
 from prep_data import *
 
-def loop_days(beg, end):
-    days_list=[]
-    for i in range(beg,end+1):
-        if i<10:
-            days_list.append("0"+str(i))
-        elif i>9 and i<32:
-            days_list.append(str(i))
-        else:
-            print("smth went wrong")
-            return 0
-    return days_list
 
 def main():
     #load data
     print("load data")
-    months=["01"]
-    #days=["01"]
-    days=loop_days(1,31)
-    for mon in months:
-        for day in days: 
-            name="2020/"+mon+"/2020-"+mon+"-"+day+"-prices.csv"
-            my_file = pathlib.Path("data/processed/"+name)
-            if not my_file.exists ():
-                print("file does NOT exist: require processing (save output to processed folder)")
-                prices_raw=pd.read_csv("data/raw/GasPrices/"+name)
-                prices = presel_data(prices_raw)
-                prices.to_csv(my_file)
-            else:
-                prices=pd.read_csv(my_file)
-                
-                print("Read {} entries for {}".format(len(prices),my_file))
-            
-            
+    prices=give_me_data()
+    
 if __name__ == "__main__":
     start = time.time()
     main()
--- prep_data.py
+++ prep_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pathlib
 
 #define geographical constraints
 minlat=53.567181
@@ -28,4 +29,36 @@ def presel_data(df):
     df_addinfo = extend_station_info(df_presel)
     #df_presel.loc[:,'station_name']=df_presel.station_uuid
     return df_addinfo 
-    
+
+def loop_days(beg, end):
+    days_list=[]
+    for i in range(beg,end+1):
+        if i<10:
+            days_list.append("0"+str(i))
+        elif i>9 and i<32:
+            days_list.append(str(i))
+        else:
+            print("smth went wrong")
+            return 0
+    return days_list
+
+
+def give_me_data():
+    months=["01"]
+    #days=["01"] day=1,month=1,year=2020
+    days=loop_days(1,31)
+    for mon in months:
+        for day in days: 
+            name="2020/"+mon+"/2020-"+mon+"-"+day+"-prices.csv"
+            my_file = pathlib.Path("data/processed/"+name)
+            if not my_file.exists ():
+                print("file does NOT exist: require processing (save output to processed folder)")
+                prices_raw=pd.read_csv("data/raw/GasPrices/"+name)
+                prices = presel_data(prices_raw)
+                prices.to_csv(my_file)
+            else:
+                prices=pd.read_csv(my_file)
+                
+                print("Read {} entries for {}".format(len(prices),my_file))
+    return prices
+
