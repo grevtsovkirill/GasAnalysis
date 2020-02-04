3a1e8f68f2aec0d5f39b863238db9595e3642297
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-04 18:13:36 +0100

    isolate plotting code

--- plot_helper.py
+++ plot_helper.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 def plot_over_time(data,gas_type='e5'):
-    df1= data.sort_values('date', ascending=True)
-    lab_str=gas_type+"_"+df1['station_name'].values[0]+"_"+df1['station_address'].values[0]
+    df1 = data.sort_values('date', ascending=True)
+    lab_str = gas_type+"_"+df1['station_name'].values[0]+"_"+df1['station_address'].values[0]
+    lab_str = lab_str.replace(" ", "")
+    print(lab_str)
     fig, ax = plt.subplots()
     ax.plot(pd.to_datetime(df1['date']), df1[gas_type],label=lab_str)
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%D'))
