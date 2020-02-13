c187b72b56f0cb44e989a6aa3241fdc01ba26a7b
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 16:15:42 +0100

    option to save model, and read from file (not to retrain all the time)

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -3,6 +3,8 @@ from model_dataprep import *
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.models import load_model
+
 import matplotlib.pyplot as plt
 
 
@@ -10,6 +12,7 @@ histdata = HistoricalData('data/processed/total_test')
 histdata.get_data_for_prediction()
 df = histdata.data_for_prediction
 print("Available data from: ",histdata.startdate," to ",histdata.stopdate)
+range_name = str(histdata.startdate)+"_"+str(histdata.stopdate)
 
 training_fraction = 0.67
 window_size = 10
@@ -20,19 +23,24 @@ trainX = np.reshape(data.X_train, (data.X_train.shape[0], 1, data.X_train.shape[
 testX = np.reshape(data.X_test, (data.X_test.shape[0], 1, data.X_test.shape[1]))
 
 do_train = True
-if do_train:
+do_load = False
+model_suf = ''
+if do_train and not do_load:
     model = Sequential()
     model.add(LSTM(4, input_shape=(1, window_size)))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     model.fit(trainX, data.Y_train, epochs=100, batch_size=1, verbose=2)
+    model.save('Models/LSTM_'+range_name+'.h5')
+    model_suf = 'train'
+if do_load:
+    model = load_model('Models/LSTM_'+range_name+'.h5')
+    model_suf = 'load'
+# make predictions using trained/loaded model:
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
     
-    # make predictions
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
-    
-    plt.plot(data.Y_train,label='Train Data')
-    plt.plot(trainPredict,label='Train Prediction')
-    plt.legend()
-    plot_name = str(histdata.startdate)+"_"+str(histdata.stopdate)
-    plt.savefig("Plots/LSTM_train_"+plot_name+".png", transparent=True)
+plt.plot(data.Y_train,label='Train Data')
+plt.plot(trainPredict,label='Train Prediction')
+plt.legend()
+plt.savefig("Plots/LSTM_train_"+range_name+model_suf+".png", transparent=True)
