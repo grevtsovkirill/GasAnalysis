ee1f585296cceaf319727a9f248c1415da8416c0
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 12:02:54 +0100

    class of historical data

--- model_dataprep.py
+++ model_dataprep.py
@@ -2,22 +2,34 @@ import pandas as pd
 import numpy as np
 import os
 
-def read_all_data(data_dir = 'data/processed/total_test', spec_id = 'e1a15081-2617-9107-e040-0b0a3dfe563c', gas_type = 'e5'):
-    '''
-    Read all available data
-    '''
-    min_price_data = pd.DataFrame()
-    for filename in os.listdir(data_dir):
-        if "csv" in filename:
-            print(filename)
-            dataset=pd.read_csv(os.path.join(data_dir, filename))
-            dataset_spec = dataset.loc[dataset.station_uuid==spec_id]
-            dataset_spec = dataset_spec.sort_values('date', ascending=True)
-            dataset_spec = dataset_spec.loc[dataset_spec[gas_type]<=dataset_spec[gas_type].min()][:1]
-            min_price_data = min_price_data.append(dataset_spec)
-
-    min_price_data = min_price_data.sort_values('date', ascending=True)
-    return min_price_data
+class HistoricalData:
+    spec_id = 'e1a15081-2617-9107-e040-0b0a3dfe563c'
+    gas_type = 'e5'
+    def __init__(self, data_dir):
+        self.data_dir=data_dir
+        
+    def read_all_data(self):
+        '''
+        Read all available data
+        '''
+        min_price_data = pd.DataFrame()
+        for filename in os.listdir(self.data_dir):
+            if "csv" in filename:
+                print(filename)
+                dataset=pd.read_csv(os.path.join(self.data_dir, filename))
+                dataset_spec = dataset.loc[dataset.station_uuid==self.spec_id]
+                dataset_spec = dataset_spec.sort_values('date', ascending=True)
+                dataset_spec = dataset_spec.loc[dataset_spec[self.gas_type]<=dataset_spec[self.gas_type].min()][:1]
+                min_price_data = min_price_data.append(dataset_spec)
+                
+        min_price_data = min_price_data.sort_values('date', ascending=True)
+        return min_price_data
+    
+    def prep_min_price(self,df):
+        df = df[['date', self.gas_type]].copy()
+        df.loc[:,'date']=pd.to_datetime(df['date'],utc=True).dt.date
+        df = df.reset_index(drop=True)
+        return df
         
 class ModelDataPrep:
     def __init__(self, df, train):
