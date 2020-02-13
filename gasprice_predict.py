6f1b0b4899aa4c41752a866cac19bb69d9d799ba
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 14:36:11 +0100

    call function from the ModelDataPrep class to get test and training samples

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -4,4 +4,8 @@ from model_dataprep import *
 rawdata = HistoricalData('data/processed/total_test')
 df0 = rawdata.read_all_data()
 df = rawdata.prep_min_price(df0)
-data = ModelDataPrep(df,1)
+training_fraction = 0.67
+window_size = 10
+data = ModelDataPrep(df,training_fraction,window_size)
+data.gen_train()
+data.gen_test()  
