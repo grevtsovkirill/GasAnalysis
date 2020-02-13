02862a7a40f91cabe0f63808d5f516ae19f2a9df
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 12:05:52 +0100

    load historical data and convert it in date:min_price format

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -1,5 +1,8 @@
 import numpy as np
 from model_dataprep import *
 
-df = read_all_data()
+rawdata = HistoricalData('data/processed/total_test')
+df0 = rawdata.read_all_data()
+df = rawdata.prep_min_price(df0)
+print(df.head())
 data = ModelDataPrep(df,1)
