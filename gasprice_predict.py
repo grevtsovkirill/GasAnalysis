c29ce52ea2d8ad6c13130766acf0f9840bc8dca2
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 14:36:45 +0100

    reshape input to match model requirements

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -9,3 +9,5 @@ window_size = 10
 data = ModelDataPrep(df,training_fraction,window_size)
 data.gen_train()
 data.gen_test()  
+trainX = np.reshape(data.X_train, (data.X_train.shape[0], 1, data.X_train.shape[1]))
+testX = np.reshape(data.X_test, (data.X_test.shape[0], 1, data.X_test.shape[1]))
