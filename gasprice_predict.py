0666c99a34598dc76587ced570d838c8145263be
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 11:41:20 +0100

    initial commit: migrate prediction model from sandbox to production

---
+++ gasprice_predict.py
@@ -0,0 +1,5 @@
+import numpy as np
+from model_dataprep import *
+
+df = pd.DataFrame()
+data = ModelDataPrep(df,1)
---
+++ model_dataprep.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+class ModelDataPrep:
+    def __init__(self, df, train):
+        self.data = df
