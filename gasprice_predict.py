561f9b911c14fd33cf85ee996bfe91aa4a1f8c50
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 14:38:56 +0100

    create and fit the LSTM network

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -1,5 +1,8 @@
 import numpy as np
 from model_dataprep import *
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
 
 rawdata = HistoricalData('data/processed/total_test')
 df0 = rawdata.read_all_data()
@@ -11,3 +14,14 @@ data.gen_train()
 data.gen_test()  
 trainX = np.reshape(data.X_train, (data.X_train.shape[0], 1, data.X_train.shape[1]))
 testX = np.reshape(data.X_test, (data.X_test.shape[0], 1, data.X_test.shape[1]))
+
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, window_size)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, data.Y_train, epochs=100, batch_size=1, verbose=2)
+
+#print(data.X_test)
+#print(data.Y_test)
+#print(trainX)
+
