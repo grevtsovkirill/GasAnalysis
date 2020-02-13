fc97a7b5c9d0b3f7502578d5cb5250663c101649
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 18:20:08 +0100

    update model including data since 2017

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Check evolution of the prices over time:
 
 Train LSTM model to predict "next-day-price":
 
-![alt text](https://github.com/grevtsovkirill/GasAnalysis/blob/master/Plots/LSTM_test_2019.png)
+![alt text](https://github.com/grevtsovkirill/GasAnalysis/blob/master/Plots/LSTM_train_2017-01-01_2020-02-12load.png)
 
 
 Predict the right time to refill:
--- gasprice_predict.py
+++ gasprice_predict.py
@@ -1,28 +1,31 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from model_dataprep import *
 from plot_helper import *
 
-histdata = HistoricalData('data/processed/total_test')
+debug = False
+do_train = True
+do_load = True
+training_fraction = 0.69
+window_size = 10
+
+
+histdata = HistoricalData('data/processed/total')
 histdata.get_data_for_prediction()
 df = histdata.data_for_prediction
 print("Available data from: ",histdata.startdate," to ",histdata.stopdate)
 range_name = str(histdata.startdate)+"_"+str(histdata.stopdate)
 
-plot_over_time(df,True,'e5_variation_'+range_name,'Price evolution of ')
+if debug:
+    plot_over_time(df,True,'e5_variation_'+range_name,'Price evolution of ')
 
-training_fraction = 0.67
-window_size = 10
 data = ModelDataPrep(df,training_fraction,window_size)
 data.gen_train()
 data.gen_test()  
 trainX = np.reshape(data.X_train, (data.X_train.shape[0], 1, data.X_train.shape[1]))
 testX = np.reshape(data.X_test, (data.X_test.shape[0], 1, data.X_test.shape[1]))
 
-debug = True
-do_train = False
-do_load = False
+
 model_suf = ''
 
 if not debug:
@@ -30,6 +33,9 @@ if not debug:
     from keras.layers import Dense
     from keras.layers import LSTM
     from keras.models import load_model
+
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
     
     if do_train and not do_load:
         model = Sequential()
@@ -47,8 +53,16 @@ if not debug:
     trainPredict = model.predict(trainX)
     testPredict = model.predict(testX)
     
-    plt.plot(data.Y_train,label='Train Data')
-    plt.plot(trainPredict,label='Train Prediction')
-    plt.legend()
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    ax1.plot(data.Y_train,label='Train Data')
+    ax1.plot(trainPredict,label='Train Prediction')
+    ax1.legend()
+
+    ax2.plot(data.Y_test,label='Test Data')
+    ax2.plot(testPredict,label='Test Prediction')
+    ax2.legend()
+    
+    plt.tight_layout()
     plt.savefig("Plots/LSTM_train_"+range_name+model_suf+".png", transparent=True)
+    #plt.savefig("Plots/LSTM_test_"+range_name+model_suf+".png", transparent=True)
 
