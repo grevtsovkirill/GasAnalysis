f9e21e099cc17a0e1a4c6587e764c86a9d9238b7
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 12:12:10 +0100

    add the range of availalbe historical data

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -4,5 +4,4 @@ from model_dataprep import *
 rawdata = HistoricalData('data/processed/total_test')
 df0 = rawdata.read_all_data()
 df = rawdata.prep_min_price(df0)
-print(df.head())
 data = ModelDataPrep(df,1)
