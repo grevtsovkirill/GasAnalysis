cc6285061ac659578b7eea62c7ba90002e89527e
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-13 17:06:30 +0100

    update helper: flexible options to save, specify name/label

--- plot_helper.py
+++ plot_helper.py
@@ -2,16 +2,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from pandas.plotting import register_matplotlib_converters
 
-def plot_over_time(data,gas_type='e5'):
+register_matplotlib_converters()
+        
+def plot_over_time(data,to_save=False,name='',lab_str='',gas_type='e5'):
     df1 = data.sort_values('date', ascending=True)
-    lab_str = gas_type+"_"+df1['station_name'].values[0]+"_"+df1['station_address'].values[0]
-    lab_str = lab_str.replace(" ", "")
-    print(lab_str)
+    lab_str = lab_str+' '+gas_type
     fig, ax = plt.subplots()
     ax.plot(pd.to_datetime(df1['date']), df1[gas_type],label=lab_str)
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%D'))
     ax.grid(True)
     ax.set_title(lab_str)
     fig.autofmt_xdate()
-    plt.savefig("Plots/overal_"+lab_str+".png", transparent=True)
+    if to_save:
+        plt.savefig("Plots/"+name+".png", transparent=True)
