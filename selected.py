e00775a0e332ba9037c23d2a674969492fdb1ed7
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-02-26 17:33:16 +0100

    define favourite stations

---
+++ selected.py
@@ -0,0 +1,22 @@
+gas_type = 'e5'
+
+fav_stations = {
+    'aral_home' :{
+        'id'   : '95b23160-eeb2-440f-adc4-245238f24e42',
+        'name' : 'ARAL Holsteiner Chaussee 55',
+        'lat'  : '53.614710',
+        'long' : '9.906061'
+        },
+    'star_home' :{
+        'id'   : '005056ba-7cb6-1ed4-8089-881fa56f0dda',
+        'name' : 'STAR Holsteiner Chaussee 182',
+        'lat'  : '53.624820',
+        'long' : '9.909542'
+        },
+    'hem_home' :{
+        'id'   : 'e1a15081-2617-9107-e040-0b0a3dfe563c',
+        'name' : 'HEM Kieler Str. 361',
+        'lat'  : '53.588750',
+        'long' : '9.930771'
+        },
+    }
