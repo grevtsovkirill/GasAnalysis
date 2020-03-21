b4fd162e5d801faa73ff72bac905f5ac76bcbc4c
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-21 15:29:29 +0100

    match time of the measurement to bin in day-hour future histogram

--- rectime.py
+++ rectime.py
@@ -6,7 +6,15 @@ from plot_helper import *
 
 st = 'star_home'
 spec_id = fav_stations[st]['id']
-
+time_split_array = pd.date_range("06:00", "23:59", freq="30min").time
+def match_time_to_index(time_v, t_array=time_split_array):
+    for j in range(len(t_array)):
+        #print(t_array[j],t_array[j+1])
+        if time_v > t_array[j]: # and time_v<
+            if time_v < t_array[j+1]:
+                #print(t_array[j],time_v)
+                return j
+            
 data_dir = 'data/processed/total'
 filename = '2020-03-11-prices.csv'
 dataset = pd.read_csv(os.path.join(data_dir, filename))
@@ -21,7 +29,7 @@ dataset_spec.reset_index(inplace=True)
 print(len(dataset_spec),dataset_spec[gas_type].min())
 for i in range(len(dataset_spec)):
     t_i, p_i = dataset_spec['date'][i],dataset_spec[gas_type][i]
-    print(i,'\t',t_i.time(),'\t',p_i)
+    print(i,'\t',t_i.time()," (bin=",match_time_to_index(t_i.time()),")",'\t',p_i)
 #print(dataset_spec[['date',gas_type]])
 #plot_over_day(dataset_spec,True,'test','day 1 ') 
 #plot_over_time(dataset_spec,True,'test','day 1 ') 
