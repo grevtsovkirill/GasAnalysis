cd615adaf53eea1969f9debdb3608b5e65914854
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-21 15:05:21 +0100

    select only price changein events

--- rectime.py
+++ rectime.py
@@ -11,13 +11,16 @@ data_dir = 'data/processed/total'
 filename = '2020-03-11-prices.csv'
 dataset = pd.read_csv(os.path.join(data_dir, filename))
 dataset_spec = dataset.loc[dataset.station_uuid==spec_id]   
-#dataset_spec = dataset_spec.loc[dataset.e5change==1]   
+dataset_spec = dataset_spec.loc[dataset.e5change==1]
+dataset_spec['date'] = pd.to_datetime(dataset_spec['date'])
 dataset_spec = dataset_spec.sort_values('date', ascending=True)
 #dataset_spec = dataset_spec.loc[dataset_spec[gas_type]<=dataset_spec[gas_type].min()][:1]
 min_val=dataset_spec[gas_type].min()
 dataset_spec[gas_type] = dataset_spec[gas_type].apply(lambda x: (x-min_val)/min_val*100 )
 
 print(len(dataset_spec),dataset_spec[gas_type].min())
-print(dataset_spec)
-plot_over_day(dataset_spec,True,'test','day 1 ') 
+for i in range(len(dataset_spec)):
+    print(dataset_spec[gas_type].loc[i+1])
+#print(dataset_spec[['date',gas_type]])
+#plot_over_day(dataset_spec,True,'test','day 1 ') 
 #plot_over_time(dataset_spec,True,'test','day 1 ') 
