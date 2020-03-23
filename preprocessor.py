368b39b6a7f49eb9844f2763989009c419d5a2c2
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-23 21:23:11 +0100

    read input arg to datetime object, propagate new format to prepare_data function

--- preprocessor.py
+++ preprocessor.py
@@ -2,6 +2,7 @@ import sys,os,time,stat,logging
 import pandas as pd
 import numpy as np
 import argparse
+import datetime
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -9,19 +10,29 @@ log = logging.getLogger(__name__)
 from prep_data import *
 from plot_helper import *
 
+def valid_date(s):
+    try:
+        return datetime.datetime.strptime(s, "%d-%m-%Y")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
 parser = argparse.ArgumentParser(description='Upload data from sensor')
 parser.add_argument('-t','--type', required=True, type=str, choices=['date', 'range'], help='Choose processing type: date or range ')
-parser.add_argument('--d0', required=True, type=str, help='Date in format DD-MM-YYYY')
+#parser.add_argument('--d0', required=True, type=str, help='Date in format DD-MM-YYYY')
+parser.add_argument('--d0', required=True, type=valid_date, help='Date in format DD-MM-YYYY')
 parser.add_argument('--d1', required=False, type=str, help='Date in format DD-MM-YYYY')
 args = parser.parse_args()
 
+    
 process_type = vars(args)["type"]
 initial_date = vars(args)["d0"]
 final_date = vars(args)["d1"]
 
-if (process_type=='range' and  not final_date ):
-    parser.error('The type range requires the --d1 argument')
-    
+if (process_type=='range'):
+    if( not final_date ):
+        parser.error('The type range requires the --d1 argument')
+
 
 
 def main():
@@ -33,9 +44,7 @@ def main():
     #        prepare_data(month,j)
     #print(process_type,initial_date)
     if process_type=='date':
-        dd, mm, yy = initial_date.split("-")
-        #print(type(dd))
-        prepare_data(dd, mm, yy)
+        prepare_data(initial_date)
     #prepare_data("02","2020")
     
 if __name__ == "__main__":
