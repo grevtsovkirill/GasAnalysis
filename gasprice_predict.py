fceb08eaa6c1868c9af322e8a0f54b36a32469b2
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-28 22:19:48 +0100

    add argparse to replace built-in arguments

--- gasprice_predict.py
+++ gasprice_predict.py
@@ -3,16 +3,26 @@ import numpy as np
 from model_dataprep import *
 from plot_helper import *
 from selected import *
+import argparse
 
-debug = True
-do_train = True
-do_load = True
-training_fraction = 0.69
-window_size = 10
+parser = argparse.ArgumentParser(description='Upload data from sensor')
+parser.add_argument('-t','--type', required=True, type=str, choices=['model_train','model_apply', 'tableau'], help='Choose type: do model training or application; get tableau data ')
+parser.add_argument('-s','--station', required=True, type=str, choices=['star_home','aral_home','hem_home'], help='Choose station ')
+parser.add_argument('--debug', required=False, default=False, type=bool, help='For local checks ')
+parser.add_argument('--tf', required=False, type=float, default=0.69, help="Fraction of used data for training")
+parser.add_argument('--win_size', required=False, type=int, default=10, help="Size of training window") 
+args = parser.parse_args()
 
-st = 'star_home'
+process_type = vars(args)["type"]
+debug = vars(args)["debug"]
+st = vars(args)["station"]
+training_fraction = vars(args)["tf"]
+window_size = vars(args)["win_size"]
+
+
+
+###
 spec_id = fav_stations[st]['id']
-#'e1a15081-2617-9107-e040-0b0a3dfe563c'
 print(fav_stations[st]['name'])
 histdata = HistoricalData('data/processed/total',spec_id)
 histdata.get_data_for_prediction()
@@ -20,7 +30,7 @@ df = histdata.data_for_prediction
 print("Available data from: ",histdata.startdate," to ",histdata.stopdate)
 range_name = st+"_"+str(histdata.startdate)+"_"+str(histdata.stopdate)
 
-if debug:
+if process_type=='tableau':
     plot_over_time(df,True,'e5_variation_'+range_name,'Price evolution of ')
     file_name = 'data/Tableau/'+range_name+'.csv'
     df.to_csv(file_name, sep=',')
@@ -34,7 +44,7 @@ testX = np.reshape(data.X_test, (data.X_test.shape[0], 1, data.X_test.shape[1]))
 
 model_suf = ''
 
-if not debug:
+if 'model' in process_type:
     from keras.models import Sequential
     from keras.layers import Dense
     from keras.layers import LSTM
@@ -43,7 +53,7 @@ if not debug:
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
     
-    if do_train and not do_load:
+    if process_type=='model_train':
         model = Sequential()
         model.add(LSTM(4, input_shape=(1, window_size)))
         model.add(Dense(1))
@@ -51,7 +61,7 @@ if not debug:
         model.fit(trainX, data.Y_train, epochs=100, batch_size=1, verbose=2)
         model.save('Models/LSTM_'+range_name+'.h5')
         model_suf = 'train'
-    if do_load:
+    elif process_type=='model_apply':
         model = load_model('Models/LSTM_'+range_name+'.h5')
         model_suf = 'load'
 
