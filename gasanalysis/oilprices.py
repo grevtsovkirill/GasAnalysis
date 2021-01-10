99dec17d2a56dca134160317c569c0d27d079596
Kirill Grevtsov <grevtsovkirill@gmail.com>
2021-01-10 23:25:14 +0100

    Extract oil historical prices

--- gasanalysis/oilprices.py
+++ gasanalysis/oilprices.py
@@ -1,13 +1,25 @@
 from datapackage import Package
+import pandas as pd
 
-#https://datahub.io/core/oil-prices#python
-package = Package('https://datahub.io/core/oil-prices/datapackage.json')
 
-# print list of all resources:
-print(package.resource_names)
+def get_prices(oil_type = "brent", do_save = True):
+    #https://datahub.io/core/oil-prices#python
+    package = Package('https://datahub.io/core/oil-prices/datapackage.json')
+    #print(package.resource_names)
+    oil_key = oil_type+"-daily"
+    for resource in package.resources:
+        if resource.descriptor['datahub']['type'] == 'derived/csv':    
+            if resource.descriptor['datahub']['derivedFrom'] == [oil_key]:
+                oil_daily = resource.read()
+                print(len(oil_daily))
+    df = pd.DataFrame(oil_daily, columns = ["Date", "Oil_Price"])
+    if do_save:
+        df.to_csv("data/oil/"+oil_type+".csv")
+    return df
 
-#print processed tabular data (if exists any)
-for resource in package.resources:
-    if resource.descriptor['datahub']['type'] == 'derived/csv':
-        print(resource.read())
-#print(package.resources['brent-daily_csv'].read())
+def main():
+    print(get_prices().head())
+
+if __name__ == "__main__":
+    main()
+    
