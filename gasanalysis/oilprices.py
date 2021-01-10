b18d2580e577901f787fc230825edb9bdb31a783
Kirill Grevtsov <grevtsovkirill@gmail.com>
2021-01-10 23:41:51 +0100

    check oil historical trends

--- gasanalysis/oilprices.py
+++ gasanalysis/oilprices.py
@@ -1,6 +1,7 @@
 from datapackage import Package
 import pandas as pd
-
+import pathlib
+import plot_helper
 
 def get_prices(oil_type = "brent", do_save = True):
     #https://datahub.io/core/oil-prices#python
@@ -12,13 +13,23 @@ def get_prices(oil_type = "brent", do_save = True):
             if resource.descriptor['datahub']['derivedFrom'] == [oil_key]:
                 oil_daily = resource.read()
                 print(len(oil_daily))
-    df = pd.DataFrame(oil_daily, columns = ["Date", "Oil_Price"])
+    df = pd.DataFrame(oil_daily, columns = ["date", "oil"])
     if do_save:
         df.to_csv("data/oil/"+oil_type+".csv")
     return df
 
+def plot_oil(oil_type = "brent"):
+    my_file = pathlib.Path("data/oil/"+oil_type+".csv")
+    if not my_file.exists ():
+        print("No oil prices found. Request from DB:")
+        df = get_prices(oil_type)
+    else:
+        df=pd.read_csv(my_file)
+    plot_helper.plot_over_time(df,True,'oil','Price evolution of ','oil')
+
 def main():
-    print(get_prices().head())
+    #print(get_prices().head())
+    plot_oil()
 
 if __name__ == "__main__":
     main()
